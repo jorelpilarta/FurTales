7f3b10ee60640339bf6a96e5ce6b237e9c5a9970
TroyZareno <f.t.zareno@gmail.com>
2017-05-14 17:31:22 +0800

    Update index.html

--- client/index.php
+++ client/index.php
@@ -117,9 +117,6 @@
       <img src="stylesheet/image/sp.jpg" class="img-rounded" alt="sp1" width="220" height="220">
 	  <h4>Health Check</h4>
 		<li>General check-up</li>
-		<li>Oral check</li>
-		<li>Vaccination</li>
-		<li>Anti-rabies injection</li>
 	  </div>
 	<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
       <img src="stylesheet/image/sp.jpg" class="img-rounded" alt="sp1" width="220" height="220">
@@ -170,113 +167,6 @@
 
 <hr></hr>
 
-<div class="container-fluid">
-<div class="row">
-<div class="col-md-6" style="background-color: d4d4d4">
-  <form class="form-horizontal" id="register" action="/action_page.php">
-    <h3>Client Registration</h3>
-    <div class="form-group">
-      <label class="control-label col-sm-3" for="Last Name">Last Name:</label>
-      <div class="col-sm-6">
-        <input type="text" class="form-control" id="rLName" placeholder="Enter last name" name="pName">
-      </div>
-    </div>
-	<div class="form-group">
-      <label class="control-label col-sm-3" for="First Name">First Name:</label>
-      <div class="col-sm-6">
-        <input type="text" class="form-control" id="rFName" placeholder="Enter first name" name="pName">
-      </div>
-    </div>
-	<div class="form-group">
-      <label class="control-label col-sm-3" for="HomeAddress">Home Address:</label>
-      <div class="col-sm-6">
-        <input type="text" class="form-control" id="Address" placeholder="Enter Address" name="Address">
-      </div>
-    </div>
-	<div class="form-group">
-      <label class="control-label col-sm-3" for="email">Email:</label>
-      <div class="col-sm-6">
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-      </div>
-	 </div>
-	  <div class="form-group">
-      <label class="control-label col-sm-3" for="Contact Number">Contact Number:</label>
-      <div class="col-sm-6">
-        <input type="email" class="form-control" id="contactNumber" placeholder="Enter Contact Number" name="ContactNumber">
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="control-label col-sm-3" for="pwd">Password:</label>
-      <div class="col-sm-6">          
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-      </div>
-    </div>
-    <div class="form-group">        
-      <div class="col-sm-offset-2 col-sm-12">
-        <button type="submit" class="btn btn-default">Submit</button>
-      </div>
-    </div>
-</div>
-</form><!--end of client registration-->
-
-<div class="col-md-6" style="background-color: d4d4d4"><!--start of sp registration-->
-  <h3>Service Provider Registration</h3>
-  <form class="form-horizontal" id="register" action="/action_page.php">
-  <div class="form-group">
-      <label class="control-label col-sm-3" for="IDNumber">ID:</label>
-      <div class="col-sm-6">
-        <input type="text" class="form-control" id="rFID" placeholder="" name="pID">
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="control-label col-sm-3" for="Last Name">Last Name:</label>
-      <div class="col-sm-6">
-        <input type="text" class="form-control" id="rFName" placeholder="Enter last name" name="pName">
-      </div>
-    </div>
-	<div class="form-group">
-      <label class="control-label col-sm-3" for="First Name">First Name:</label>
-      <div class="col-sm-6">
-        <input type="text" class="form-control" id="rLName" placeholder="Enter first name" name="pName">
-      </div>
-    </div>
-	<div class="form-group">
-      <label class="control-label col-sm-3" for="sID">Service ID:</label>
-      <div class="col-sm-6">
-        <input type="text" class="form-control" id="#" placeholder="" name="sID">
-      </div>
-    </div>
-	<div class="form-group">
-      <label class="control-label col-sm-3" for="email">Email:</label>
-      <div class="col-sm-6">
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-      </div>
-    </div>
-	<div class="form-group">
-      <label class="control-label col-sm-3" for="Contact Number">Contact Number:</label>
-      <div class="col-sm-6">
-        <input type="email" class="form-control" id="contactNumber" placeholder="Enter Contact Number" name="ContactNumber">
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="control-label col-sm-3" for="pwd">Password:</label>
-      <div class="col-sm-6">          
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-      </div>
-    </div>
-    <div class="form-group">        
-      <div class="col-sm-offset-2 col-sm-12">
-        <button type="submit" class="btn btn-default">Submit</button>
-      </div>
-    </div>
-</div>
-</form>
-
-
-
-</div><!--end of class row-->
-</div><!--end of log in source code-->
-<hr></hr>
 <div class="container-fluid">
   <div class="panel panel-default">
     <div class="panel-footer"><p class="text-center"><span class="glyphicon glyphicon-copyright-mark">2017 Furtales Pet Service, Inc. All rights reserved. Furtales.com is a trademark of Furtales Pet Services Co.</span></p></div>
